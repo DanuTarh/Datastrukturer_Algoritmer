d05af8ebdb7707a5cac89e45ea0233a8fadf8866
tarhoned <tarhoned@arcada.fi>
2025-01-09 11:34:28 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+
+    // vi deklarerar string-variabeln name
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+     }
+
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("jajajajaaja");
+
+        Car car = new Car("Toyota");
+        Car otherCar = new Car("volkswagen");
+        System.out.println("Let the battle begin "
+                + car.getName() + " " + otherCar.getName());
+        car.setName("KIA");
+        System.out.println("New name : " + car.getName());
+
+
 
     }
 }
